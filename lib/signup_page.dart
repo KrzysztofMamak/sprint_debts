533595fd3e648feea290b0b31b3dcd7a2caceb9f
KrzysztofMamak <mamako@onet.pl>
2022-07-04 22:42:30 +0200

    Wrap SignupPage in Scaffold

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -5,6 +5,11 @@ class SignupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signup'),
+      ),
+      body: Container(color: Colors.red),
+    );
   }
 }
