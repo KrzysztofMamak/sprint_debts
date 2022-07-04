f9861c96f211955db815e7e0c88338df42cdf85f
KrzysztofMamak <mamako@onet.pl>
2022-07-04 21:19:54 +0200

    Move LoggerModule under module dir

--- lib/injection/injection.config.dart
+++ lib/injection/injection.config.dart
@@ -21,7 +21,7 @@ import '../local_auth_repository.dart' as _i12;
 import '../remote_auth_repository.dart' as _i7;
 import '../sign_in_form_bloc/sign_in_form_bloc.dart' as _i15;
 import '../signup_form_bloc/signup_form_bloc.dart' as _i16;
-import 'logger_module.dart' as _i18;
+import 'module/logger_module.dart' as _i18;
 import 'module/dio_module.dart' as _i17;
 import 'module/shared_preferences_module.dart'
     as _i19; // ignore_for_file: unnecessary_lambdas
