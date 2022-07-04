351d16ac3a169fde6393a6225abeba43e5fd9666
KrzysztofMamak <mamako@onet.pl>
2022-07-04 22:40:39 +0200

    Add test implementation of the onNavigation method

--- lib/auth_guard.dart
+++ lib/auth_guard.dart
@@ -1,10 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:injectable/injectable.dart';
+import 'package:sprint_debts/app_router.dart';
 
 @injectable
 class AuthGuard extends AutoRouteGuard {
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) {
-    resolver.next();
+    // TODO -> implement
+    if (true) {
+      router.replace(const SignInPageRoute());
+    }
   }
 }
